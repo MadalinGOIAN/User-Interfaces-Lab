0d859fbb771b9360deb96da1b6f7a7d60801f7c6
Mădălin-Florin GOIAN <79133273+MadalinGOIAN@users.noreply.github.com>
2023-11-25 00:06:18 +0200

    Add search functionality in Home Page

--- js/index.js
+++ js/index.js
@@ -17,3 +17,81 @@ function ToggleDarkMode() {
 
     document.getElementById("go-top-button").classList.toggle("dark-mode");
 }
+
+function HighlightSearchResults() {
+    var searchQuery = document.querySelector('input[type=search]').value.toLowerCase();
+    var cards = document.querySelectorAll('.card');
+    var i = 0;
+
+    cards.forEach(function(card) {
+        var cardText = card.textContent.toLowerCase();
+        var startIndex = cardText.indexOf(searchQuery);
+
+        if (startIndex !== -1) {
+            if (i == 0) {
+                var highlightedText = "<div class=\"card\">" + 
+                                      "<img class=\"card-img-top\" src=\"res/img/games/Doom.jpeg\" alt=\"Card image cap\">" +
+                                      "<div class=\"card-body\">" +
+                                      "<p class=\"card-text game-title\">" +
+                                      cardText.substring(0, startIndex) +
+                                      "<span class='highlighted'>" +
+                                      cardText.substring(startIndex, startIndex + searchQuery.length) +
+                                      "</span>" +
+                                      cardText.substring(startIndex + searchQuery.length) +
+                                      "</p></div></div>";
+            }
+
+            card.innerHTML = highlightedText;
+        }
+    });
+}
+
+function ClearHighlight() {
+    var cards = document.querySelectorAll('.card');
+    var i = 0;
+
+    cards.forEach(function(card) {
+        if (i == 0) {
+            card.innerHTML =
+            "<div class=\"card\">" +
+                "<img class=\"card-img-top\" src=\"res/img/games/Fortnite.jpg\" alt=\"Card image cap\">" +
+                "<div class=\"card-body\">" +
+                    "<p class=\"card-text game-title\">Fortnite</p>" +
+                    "<p class=\"card-text game-developer\">Epic Games</p>" +
+                "</div>" +
+            "</div>";
+        }
+        else if (i == 1) {
+            card.innerHTML =
+            "<div class=\"card\">" +
+                "<img class=\"card-img-top\" src=\"res/img/games/Doom.jpeg\" alt=\"Card image cap\">" +
+                "<div class=\"card-body\">" +
+                    "<p class=\"card-text game-title\">Doom</p>" +
+                    "<p class=\"card-text game-developer\">Bethesda</p>" +
+                "</div>" +
+            "</div>";
+        }
+        else if (i == 2) {
+            card.innerHTML =
+            "<div class=\"card\">" +
+                "<img class=\"card-img-top\" src=\"res/img/games/Portal2.jpg\" alt=\"Card image cap\">" +
+                "<div class=\"card-body\">" +
+                    "<p class=\"card-text game-title\">Portal 2</p>" +
+                    "<p class=\"card-text game-developer\">Valve</p>" +
+                "</div>" +
+            "</div>";
+        }
+        else if (i == 3) {
+            card.innerHTML =
+            "<div class=\"card\">" +
+                "<img class=\"card-img-top\" src=\"res/img/games/Sims4.jpg\" alt=\"Card image cap\">" +
+                "<div class=\"card-body\">" +
+                    "<p class=\"card-text game-title\">The Sims 4</p>" +
+                    "<p class=\"card-text game-developer\">EA</p>" +
+                "</div>" +
+            "</div>";
+        }
+
+        i++;
+    });
+}
